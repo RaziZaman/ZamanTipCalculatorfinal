578fc8f4e5d166a3f7a89c65955bae4f08105bf9
BT_1N3_21 <raziz299@nycstudents.net>
2023-09-21 08:35:15 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the tip calculator!");
+        System.out.print("What's the tip percentage? (0-100): ");
+        double percentage = scan.nextDouble();
+        double total = 0;
+        double cost = 0;
+        while (cost != -1);
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+            cost = scan.nextDouble();
+            if (cost != -1) {
+                total += cost;
+        }
+    }
+}
