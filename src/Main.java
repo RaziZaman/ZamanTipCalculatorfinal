a970b5caf3ba33f71231089fae5f2d00bb765b93
BT_1N3_21 <raziz299@nycstudents.net>
2023-09-21 09:02:34 -0400

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,29 @@ public class Main {
         double percentage = scan.nextDouble();
         double total = 0;
         double cost = 0;
+        String item = "";
+        String item2 = "";
         while (cost != -1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             cost = scan.nextDouble();
             if (cost != -1) {
+                scan.nextLine();
                 System.out.print("Enter the item: ");
-                String Item = scan.nextString();
-
+                item2 = scan.nextLine();
+                item = item + " " + item2;
                 total += cost;
             }
         }
-        System.out.println(total);
+        System.out.println("---------------------------------");
+        System.out.println("Total bill before tip: $" + total);
+        System.out.println("Total percentage: " + percentage + "%");
+        System.out.println("Total tip: $");
+        System.out.println("Total bill with tip: $");
+        System.out.println("Per person per cost before tip: $";
+        System.out.println("Tip per person: $");
+        System.out.println("Total cost per person: $");
+        System.out.println("---------------------------------");
+        System.out.println("Items ordered: ");
+        System.out.println(item);
     }
 }
