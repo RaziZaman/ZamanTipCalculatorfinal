c8e64d13a9702a749e7525093bb58ad5fafd93d1
BT_1N3_21 <raziz299@nycstudents.net>
2023-09-29 09:28:47 -0400

    2

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,51 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to the tip calculator!");
-        System.out.print("How many people are in your group: ");
-        int people = scan.nextInt();
-        System.out.print("What's the tip percentage? (0-100): ");
-        double percentage = scan.nextDouble();
-        percentage *= 0.01;
+        System.out.print("How much money are you able to spend today? ");
+        double budget = scan.nextInt();
         double total = 0.0;
-        double cost = 0.0;
-        String item = "";
-        String item2 = "";
-        while (cost != -1) {
-            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
-            cost = scan.nextDouble();
-            if (cost != -1) {
-                scan.nextLine();
-                System.out.print("Enter the item: ");
-                item2 = scan.nextLine();
-                if (item == "") {
-                    item = item2;
-                } else {
-                    item = item + "\n" + item2;
+        double tip = 0.0;
+            System.out.println("Welcome to the tip calculator!");
+            System.out.print("How many people are in your group: ");
+            int people = scan.nextInt();
+            System.out.print("What's the tip percentage? (0-100): ");
+            double percentage = scan.nextDouble();
+            percentage *= 0.01;
+            total = 0.0;
+            double cost = 0.0;
+            String item = "";
+            String item2 = "";
+            while (cost != -1) {
+                System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+                cost = scan.nextDouble();
+                if (cost != -1) {
+                    scan.nextLine();
+                    System.out.print("Enter the item: ");
+                    item2 = scan.nextLine();
+                    if (item == "") {
+                        item = item2;
+                    } else {
+                        item = item + "\n" + item2;
+                    }
+                    total += cost;
                 }
-                total += cost;
             }
-        }
-        double tip = (((int) ((percentage * total) * 100.0)) / 100.0);
-        System.out.println(tip);
-        System.out.println("---------------------------------");
-        System.out.println("Total bill before tip: $" + total);
-        System.out.println("Total percentage: " + (percentage * 100) + "%");
-        System.out.println("Total tip: $" + tip);
-        System.out.println("Total bill with tip: $" + (total + tip));
-        System.out.println("Per person per cost before tip: $" + (((int) ((total / people) * 100.0)) / 100.0));
-        System.out.println("Tip per person: $" + (((int) ((tip / people) * 100.0)) / 100.0));
-        System.out.println("Total cost per person: $" + (((int) (((total + tip) / people) * 100.0)) / 100.0));
-        System.out.println("---------------------------------");
-        System.out.println("Items ordered: ");
-        System.out.println(item);
+            tip = (((int) ((percentage * total) * 100.0)) / 100.0);
+            if ((total + tip) <= budget) {
+                System.out.println(tip);
+                System.out.println("---------------------------------");
+                System.out.println("Total bill before tip: $" + total);
+                System.out.println("Total percentage: " + (percentage * 100) + "%");
+                System.out.println("Total tip: $" + tip);
+                System.out.println("Total bill with tip: $" + (total + tip));
+                System.out.println("Per person per cost before tip: $" + (((int) ((total / people) * 100.0)) / 100.0));
+                System.out.println("Tip per person: $" + (((int) ((tip / people) * 100.0)) / 100.0));
+                System.out.println("Total cost per person: $" + (((int) (((total + tip) / people) * 100.0)) / 100.0));
+                System.out.println("---------------------------------");
+                System.out.println("Items ordered: ");
+                System.out.println(item);
+            } else {
+                System.out.println("You went over your budget, your total was " + (total + tip) + ", please try again.");
+            }
     }
 }
